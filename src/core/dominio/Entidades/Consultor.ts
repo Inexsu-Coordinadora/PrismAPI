0e642f3510fd756c79351300253afbbfba7e2962
Melissa Martínez <mmartineza.1897@gmail.com>
2025-10-31 15:58:16 -0500

    fix(dominio): corregir tipo e id en IConsultor y Consultor

--- src/core/dominio/Entidades/Consultor.ts
+++ src/core/dominio/Entidades/Consultor.ts
@@ -1,15 +1,13 @@
-import {IConsultor} from "../IConsultor";
+import {IConsultor, disponibilidad_consultor} from "../IConsultor";
 
 export class Consultor implements IConsultor {
-    idConsultor: string;
     nombreConsultor: string;
     especialidadConsultor: string;
-    disponibilidadConsultor: string;
+    disponibilidadConsultor: disponibilidad_consultor;
     emailConsultor: string;
     telefonoConsultor?: string | null;
 
     constructor(datosConsultor:IConsultor){
-        this.idConsultor = datosConsultor.idConsultor;
         this.nombreConsultor = datosConsultor.nombreConsultor;
         this.especialidadConsultor = datosConsultor.especialidadConsultor;
         this.disponibilidadConsultor = datosConsultor.disponibilidadConsultor;
--- src/core/dominio/IConsultor.ts
+++ src/core/dominio/IConsultor.ts
@@ -1,8 +1,10 @@
+export enum disponibilidad_consultor {'disponible', 'ocupado', 'no disponible'};
+
 export interface IConsultor {
-    idConsultor: string;
+    idConsultor?: string;
     nombreConsultor: string;
     especialidadConsultor: string;
-    disponibilidadConsultor: string;
+    disponibilidadConsultor: disponibilidad_consultor;
     emailConsultor: string;
     telefonoConsultor?: string | null;
 }
