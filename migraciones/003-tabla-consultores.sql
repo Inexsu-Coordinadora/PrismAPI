ee7f401c7d35f16539cb3ecd0f67ea2cf18fdf25
Melissa Martínez <mmartineza.1897@gmail.com>
2025-11-04 16:13:16 -0500

    fix(bd):elimina Pool y ajusta enum y propiedades en tabla 003

--- migraciones/003-tabla-consultores.sql
+++ migraciones/003-tabla-consultores.sql
@@ -1,11 +1,15 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE TYPE DisponibilidadConsultor AS ENUM ('disponible', 'ocupado', 'en descanso', 'no disponible');
-
-CREATE TABLE IF NOT EXISTS consultor (
-    idConsultor UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    nombreConsultor VARCHAR(100) NOT NULL,
-    especialidadConsultor VARCHAR(100) NOT NULL,
-    disponibilidadConsultor DisponibilidadConsultor NOT NULL, 
-    emailConsultor VARCHAR(100) NOT NULL UNIQUE,
-    telefonoConsultor VARCHAR(15) NULL
+export enum Disponibilidad_consultor {
+  DISPONIBLE = "disponible",
+  OCUPADO = "ocupado",
+  EN_DESCANSO = "en_descanso",
+  NO_DISPONIBLE = "no_disponible",
+}
+CREATE TABLE IF NOT EXISTS consultores (
+    id_consultor UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    nombre_consultor VARCHAR(100) NOT NULL,
+    especialidad_consultor VARCHAR(100) NOT NULL,
+    disponibilidad_consultor disponibilidad_consultor NOT NULL, 
+    email_consultor VARCHAR(100) NOT NULL UNIQUE,
+    telefono_consultor VARCHAR(15) NULL
 );
