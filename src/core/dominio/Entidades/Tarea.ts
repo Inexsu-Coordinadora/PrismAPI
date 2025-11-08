7dc277666ddf4b11aae855b0579474a6e2bcb814
moniGitliz <moniliz.9012@gmail.com>
2025-11-08 13:11:11 -0500

    feat(dominio): expandir entidad Tarea (migracion y dominio)

--- src/core/dominio/Entidades/Tarea.ts
+++ src/core/dominio/Entidades/Tarea.ts
@@ -5,9 +5,19 @@ export class Tarea implements ITarea{
     descripcionTarea?: string | null;
     estadoTarea: EstadoTarea;
 
+    //*-- Nuevos campos requeridos por la E2 Servicio 4 --
+    idProyecto: string;
+    idConsultorAsignado?: string | null;
+    fechaLimite?: Date | null;
+
     constructor(datosTarea : ITarea){
         this.tituloTarea = datosTarea.tituloTarea;
         this.descripcionTarea = datosTarea.descripcionTarea ?? null;
         this.estadoTarea = datosTarea.estadoTarea;
+
+        //* --- Inicializamos los nuevos campos ---
+        this.idProyecto = datosTarea.idProyecto;
+        this.idConsultorAsignado = datosTarea.idConsultorAsignado ?? null;
+        this.fechaLimite = datosTarea.fechaLimite ?? null;
     }
 }
