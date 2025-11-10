4f3cd67ab041d068c26457732be6523986ff300e
Corazonjesus2019 <stefanycs91@hotmail.com>
2025-11-10 16:26:06 -0500

    fix(infra):corrige dirección de importación

--- src/core/infraestructura/postgres/ClientePostgres.ts
+++ src/core/infraestructura/postgres/ClientePostgres.ts
@@ -1,5 +1,5 @@
 import { Pool } from "pg";
-import { configuration } from "../../common/configuracion";
+import { configuration } from "../../../common/configuracion";
 
 const pool = new Pool({
 host: configuration.baseDatos.host,
