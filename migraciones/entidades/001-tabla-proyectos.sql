fd11fb78f2c3172b578cea3dbd719381753becf2
Corazonjesus2019 <stefanycs91@hotmail.com>
2025-11-11 10:21:20 -0500

    fix(config):corrregir bug de conexión a BD por orden de carga

--- migraciones/entidades/001-tabla-proyectos.sql
+++ migraciones/entidades/001-tabla-proyectos.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS proyectos (
     id_proyecto UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     nombre_proyecto VARCHAR(100) NOT NULL,
     tipo_proyecto VARCHAR(100) NULL,
-    fecha_inicio DATE NOT NULL,
-    fecha_fin DATE NULL,
+    fecha_inicio_proyecto DATE NOT NULL,
+    fecha_fin_proyecto DATE NULL,
     estado_proyecto VARCHAR(20) NOT NULL
 );
 
--- src/core/presentacion/app.ts
+++ src/core/presentacion/app.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify';;
 import {FastifyError} from 'fastify';
+import { getConfig } from "../../common/configuracion";
 
 //* Aqui importamos los enrutadores de todas las entidades
 import { construirClienteEnrutador } from "./rutas/entidades/clienteEnrutador";
@@ -37,8 +38,12 @@ async (appInstance) => {
 );
 
 export const startServer = async (): Promise<void> => {
+  // ¡Llama a la función!
+const config = getConfig();
+
 try {
-    await app.listen({ port: Number(process.env.PUERTO) });
+    // ¡Usa la config fresca!
+    await app.listen({ port: config.httpPuerto });
     app.log.info("El servidor esta corriendo...");
 } catch (err) {
     app.log.error(`Error al ejecutar el servidor\n ${err}`);
@@ -49,7 +54,6 @@ try {
     statusCode: 500,
     message: `El servidor no se pudo iniciar: ${(err as Error).message}`,
     };
-
     throw serverError;
 }
 };
