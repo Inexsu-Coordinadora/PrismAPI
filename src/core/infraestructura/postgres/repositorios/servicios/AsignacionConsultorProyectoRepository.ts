6469e07d38902539e7c9ad682d10594e7006d02e
Corazonjesus2019 <stefanycs91@hotmail.com>
2025-11-12 16:43:55 -0500

    fix(infra): modifica query obtener asignación existente

--- src/core/infraestructura/postgres/repositorios/servicios/AsignacionConsultorProyectoRepository.ts
+++ src/core/infraestructura/postgres/repositorios/servicios/AsignacionConsultorProyectoRepository.ts
@@ -62,12 +62,17 @@ export class AsignacionConsultorProyectoRepository implements IAsignacionConsult
     }
 
     async obtenerAsignacionExistente(idConsultor: string, idProyecto: string, rolConsultor: string | null): Promise<IAsignacionConsultorProyecto | null> {
-        const query = `
+        let query = `
             SELECT * FROM asignaciones_consultores_proyectos 
-            WHERE id_consultor = $1 AND id_proyecto = $2 AND rol_consultor = $3
+            WHERE id_consultor = $1 AND id_proyecto = $2
         `;
         
-        const parametros = [idConsultor, idProyecto, rolConsultor];
+        const parametros = [idConsultor, idProyecto];
+        
+        if (rolConsultor) {
+        query += ` AND rol_consultor = $3`;
+        parametros.push(rolConsultor);
+    }
         const result = await ejecutarConsulta(query, parametros);
 
         if (result.rows.length === 0) {
